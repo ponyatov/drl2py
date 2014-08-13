2226f809ea8e0949847beec6045a9712e8a6c534
Dmitry Ponyatov <dponyatov@gmail.com>
2014-08-13 13:06:47 +0400

    + program begin
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- drl2nc.py
+++ drl2nc.py
@@ -1,2 +1,11 @@
 
+S_FILE = 'Through.drl'
+T_FILE = S_FILE+'.nc'
 
+S_FILE_H = open(S_FILE) ; print 'drill:',S_FILE
+T_FILE_H = open(T_FILE,'w') ; print 'gcode:',T_FILE
+
+S_FILE_H.close()
+T_FILE_H.close()
+
+print '.'
